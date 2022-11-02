6aea51d767c8ae56d28e4d2f8a198d8a1c7cd1ef
“ginabeki” <ginabeki3@gmail.com>
2022-11-02 16:24:43 +0300

    Set up webpack

---
+++ dist/print.bundle.js
@@ -0,0 +1,26 @@
+"use strict";
+(self["webpackChunkset_up_webpack"] = self["webpackChunkset_up_webpack"] || []).push([["print"],{
+
+/***/ "./src/print.js":
+/*!**********************!*\
+  !*** ./src/print.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ printMe)
+/* harmony export */ });
+function printMe() {
+  console.log('I get called from print.js!');
+}
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/print.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJpbnQuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7QUFBZSxTQUFTQSxPQUFPLEdBQUc7RUFDOUJDLE9BQU8sQ0FBQ0MsR0FBRyxDQUFDLDZCQUE2QixDQUFDO0FBQzVDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vc2V0LXVwLXdlYnBhY2svLi9zcmMvcHJpbnQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gcHJpbnRNZSgpIHtcbiAgICBjb25zb2xlLmxvZygnSSBnZXQgY2FsbGVkIGZyb20gcHJpbnQuanMhJyk7XG4gIH0iXSwibmFtZXMiOlsicHJpbnRNZSIsImNvbnNvbGUiLCJsb2ciXSwic291cmNlUm9vdCI6IiJ9
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+// Webpack uses this to work with directories
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// This is the main configuration object.
+// Here, you write different options and tell Webpack what to do
+module.exports = {
+
+  // Path to your entry point. From this file Webpack will begin its work
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Wbpack Exercise',
+    }),
+  ],
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+    filename: '[name].bundle.js',
+    // eslint-disable-next-line no-dupe-keys
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        // Now we apply rule for images
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            // Using file-loader for these files
+            loader: 'file-loader',
+
+            // In options we can set different things like format
+            // and directory to save
+            options: {
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on the final bundle. For now, we don't need production's JavaScript
+  // minifying and other things, so let's set mode to development
+  mode: 'development',
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
